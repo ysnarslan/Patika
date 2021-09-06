8ab906ea95f636c86a9c8184067a7df46f398d70
Yasin Arslan <ysnarsln1730@gmail.com>
2021-09-06 18:39:44 +0300

    proje.py
    Patikanın python eğitimindeki projeler.

---
+++ proje.py
@@ -0,0 +1,28 @@
+def flatten(nested_list, out=None):
+    if out is None:
+        out = []
+    for i in nested_list:
+        if (type(i) == list):
+            flatten(i, out)
+        else:
+            out.append(i)
+    return out
+
+
+def my_reverse(input_list):
+    input_list.reverse()
+    for i in input_list:
+        if (type(i) == list):
+            my_reverse(i)
+
+
+if __name__ == "__main__":
+
+    deneme = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
+    flatten_list = flatten(deneme)
+    print(flatten_list)
+
+
+    deneme2 = [[1, 2], [3, 4], [[5, [6], 7], 8], 9]
+    my_reverse(deneme2)
+    print(deneme2)
